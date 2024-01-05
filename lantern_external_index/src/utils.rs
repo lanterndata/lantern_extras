f43941de8bdd86a29bbcfea327edfd5ccc5d20f6
Varik Matevosyan <varikmatevosyan@gmail.com>
2024-01-05 16:03:59 +0400

    Save index file in postgres data dir

--- lantern_external_index/src/utils.rs
+++ lantern_external_index/src/utils.rs
@@ -1,3 +1,5 @@
+use std::{fs, path::Path};
+
 pub fn bytes_to_integer_le<T>(bytes: &[u8]) -> T
 where
     T: From<u8> + std::ops::Shl<usize, Output = T> + std::ops::BitOr<Output = T> + Default,
@@ -9,4 +11,10 @@ where
     }
 
     result
+}
+
+pub fn create_index_dir(data_dir: &str) -> Result<String, anyhow::Error> {
+    let path = Path::new(data_dir).join("ldb_indexes");
+    fs::create_dir_all(&path)?;
+    Ok(path.to_str().unwrap().to_owned())
 }
